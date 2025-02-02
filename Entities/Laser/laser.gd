6b42487bb1d65e4b1c58318a0705cf0a4b58553d
MarcosPR <marcos.perez.rus@gmail.com>
2025-02-02 22:49:29 +0100

    Upgraded visuals

--- Entities/Laser/laser.gd
+++ Entities/Laser/laser.gd
@@ -1,5 +1,6 @@
 extends Line2D
 
+
 func set_objective_point() -> void:
 	var local_mining_point = to_local(Global.mining_point)
 	var local_emitter_point = 32 * local_mining_point.normalized()
@@ -14,12 +15,20 @@ func set_objective_point() -> void:
 	$MiningParticles.position = local_mining_point
 
 func stop_laser() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "width", 0, 0.1)
 	$Emitterparticles.emitting = false
 	$MiningParticles.emitting = false
-	self.hide()
+	$MiningLight.enabled = false
+	$EmitterLight.enabled = false
+	#self.hide()
 	
 
 func start_laser() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "width", 5, 0.1)
 	$Emitterparticles.emitting = true
 	$MiningParticles.emitting = true
-	self.show()
+	$MiningLight.enabled = true
+	$EmitterLight.enabled = true
+	#self.show()
--- Entities/Mining Poly/mining_poly.gd
+++ Entities/Mining Poly/mining_poly.gd
@@ -1,14 +1,24 @@
 extends Polygon2D
 
 
+var aux_polygon : PackedVector2Array
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	aux_polygon.resize(36)
+	
+	var polygon_size := aux_polygon.size()
+	print(polygon_size)
+	var ang := 0
+	
+	for i in range(polygon_size):
+		var x := cos(deg_to_rad(ang))
+		var y := sin(deg_to_rad(ang))
+		
+		aux_polygon[i] = Global.mining_poly_radius * Vector2(x, y)
+		ang += 360/polygon_size
+		print(i)
+	
+	polygon = aux_polygon
 	Global.mining_poly = self
-	
-	var polygon_size := self.polygon.size()
-	var ang := 180
-	
-	for i in polygon_size:
-		#polygon[i] = Global.mining_poly_radius * Vector2(cos(deg_to_rad(ang)), 0.5+sin(deg_to_rad(ang))) 
-		polygon[i] = Global.mining_poly_radius * polygon[i]
-		ang += 180/(polygon_size-1)
